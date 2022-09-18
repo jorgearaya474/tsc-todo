e0ffae61914d4bfb6d79a9d55f9cea74a58630d5
Jorge Araya <jorgearaya474@gmail.com>
2022-09-17 20:41:28 -0600

    add documentation

--- src/todoItem.ts
+++ src/todoItem.ts
@@ -1,5 +1,17 @@
+/**
+ * TodoItem class
+ * @author Jorge Araya
+ * @since 1.0.0
+ */
+
 export class TodoItem {
     
+    /**
+     * Class constructor
+     * @param id 
+     * @param task 
+     * @param complete 
+     */
     constructor(
         public id: number,
         public task: string,
@@ -9,6 +21,9 @@ export class TodoItem {
         // no statements required
     }
 
+    /**
+     * Print todo method
+     */
     printDetails() : void {
         console.log(`${this.id}\t${this.task} ${this.complete ? "\t(complete)" : ""}`);
     }    
