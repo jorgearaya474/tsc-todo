92f63d87f88cd9b25066cb7b08709423ca9a01fa
Jorge Araya <jorgearaya474@gmail.com>
2022-09-17 20:40:47 -0600

    add remove complete todos method

--- src/todoCollection.ts
+++ src/todoCollection.ts
@@ -1,9 +1,28 @@
+/**
+ * TodoCollection class
+ * @author Jorge Araya
+ * @since 1.0.0
+ */
+
 import { TodoItem } from "./todoItem";
+
+/**
+ * item counts alias
+ */
+type itemCounts = {
+    total: number,
+    incomplete: number
+}
 
 export class TodoCollection{
     private nextId: number = 1;
     private itemMap = new Map<number, TodoItem>();
 
+    /**
+     * Class constructor
+     * @param userName 
+     * @param todoItems 
+     */
     constructor(
         public userName: string,
         public todoItems: TodoItem[] = [],
@@ -11,6 +30,11 @@ export class TodoCollection{
         todoItems.forEach(item => this.itemMap.set(item.id, item));
     }
 
+    /**
+     * Add todo method
+     * @param task 
+     * @returns number
+     */
     addTodo(task: string): number{
         while(this.getTodoById(this.nextId)){
             this.nextId++;
@@ -21,16 +45,51 @@ export class TodoCollection{
         return this.nextId;
     }
 
+    /**
+     * Get todo by id method
+     * @param id 
+     * @returns TodoItem
+     */
     getTodoById(id: number): TodoItem{
         return this.itemMap.get(id);
     }
 
+    /**
+     * Get todos method
+     * @param includeComplete 
+     * @returns Todoitem array 
+     */
     getTodoItems(includeComplete: boolean): TodoItem[] {
         return [...this.itemMap.values()].filter(item => includeComplete || !item.complete);
     }
 
+    /**
+     * Mark todo as complete method
+     * @param id 
+     * @param complete 
+     */
     markComplete(id: number, complete: boolean){
         const todoItem = this.getTodoById(id);
         if (todoItem) todoItem.complete = complete;
+    }
+
+    /**
+     * Remove complete todos mtehod
+     */
+    removeComplete() {
+        this.itemMap.forEach(item => {
+            if(item.complete) this.itemMap.delete(item.id);
+        });
+    }
+
+    /**
+     * Get items count
+     * @returns Object itemCounts
+     */
+    getITemCounts(): itemCounts {
+        return {
+            total: this.itemMap.size,
+            incomplete: this.getTodoItems(false).length
+        };
     }
 }
